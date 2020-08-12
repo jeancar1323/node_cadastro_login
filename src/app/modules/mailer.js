69ec5e6575a8330619d4fd4063171fa251d588b4
jeancar <jeancar1323@gmail.com>
2020-08-12 05:31:27 -0300

    Update 2

--- src/app/modules/mailer.js
+++ src/app/modules/mailer.js
@@ -1,25 +1,53 @@
 const nodemailer = require('nodemailer')
 const path = require('path')
 const hbs = require('nodemailer-express-handlebars')
+const { text } = require('express')
 
-const { host, port, user, pass, } = require('../../config/mail.json')
+// const { host, port, user, pass, } = require('../../config/mail.json')
+ const {user, pass, email, } = require('../../config/configPessoal.json')
+// const transport = nodemailer.createTransport({
+//   host,
+//   port,
+//   auth: { user, pass }
 
-const transport = nodemailer.createTransport({
-  host,
-  port,
-  auth: { user, pass }
+// });
 
+
+
+let transport = nodemailer.createTransport({
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
+  requireTLS: true,
+  auth: {
+      user,
+      pass,
+  }
 });
 
 
-transport.use('compile', hbs({
-  viewEngine: {
-    defaultLayout: undefined,
-    partialsDir: path.resolve('./src/app/resources/mail/')
-  },
-  viewPath: path.resolve('./src/app/resources/mail/'),
-  extName: '.html',
-}));
+
+// transport.sendMail(mailoptions, (error,info)=>{
+//   if (error) {
+//         console.log(error);
+//       } else {
+//         console.log('Email sent: ' + info.response);
+//       }
+// })
+
+
+
+
+
+
+// transport.use('compile', hbs({
+//   viewEngine: {
+//     defaultLayout: undefined,
+//     partialsDir: path.resolve('./src/app/resources/mail/')
+//   },
+//   viewPath: path.resolve('./src/app/resources/mail/'),
+//   extName: '.html',
+// }));
 
 
 
--- src/index.js
+++ src/index.js
@@ -3,14 +3,35 @@ const bodyparser = require('body-parser')
 const app = express();
 const authRouter = require('./app/routes/authRouter')
 const projectRouter = require('./app/routes/projectRouts')
+const mail = require('./app/modules/mailer');
+const transport = require('./app/modules/mailer');
+const { user, pass, email, } = require('./config/configPessoal.json')
 app.use(bodyparser.json())
-app.use(bodyparser.urlencoded({extended: false}))
+app.use(bodyparser.urlencoded({ extended: false }))
 
+var mailoptions = {
+      from: user,
+      to: email,
+      subject: 'testando',
+      text: "teste teste"
+}
+
+
+app.get('/email', async (req, res) => {
+
+      await transport.sendMail(mailoptions)
+            .then((response) =>       
+            res.status(200).send({
+                  Mensagem: "Email enviado com sucesso",
+                  Informacao: response.response
+            }))
+            .catch(error => res.status(400).send({error}))
+})
 
 app.use('/auth', authRouter)
 app.use('/project', projectRouter)
-app.get('/', (req,res)=>{
-      res.send('Hell world')        
+app.get('/', (req, res) => {
+      res.send('Hell world')
 })
 
 app.listen(3000, _ => console.log("Workando ?"))
